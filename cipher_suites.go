7fcdb1ef036eda91539fc5842acb2dfcb9fc2ca3
patrini32 <171664803+patrini32@users.noreply.github.com>
2026-06-04 23:45:12 +0300

    fix

--- cipher_suites.go
+++ cipher_suites.go
@@ -18,7 +18,7 @@ import (
 	"runtime"
 	_ "unsafe" // for linkname
 
-	"github.com/patrini32/utls/utls/internal/boring"
+	"github.com/patrini32/utls/internal/boring"
 	"golang.org/x/sys/cpu"
 
 	"golang.org/x/crypto/chacha20poly1305"
